03c3bd9609199ef6927dba7c3bb799e3efd78560
Unknown <enrico.morotti90@gmail.com>
2018-08-01 20:33:41 +0200

    API library setup

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import {create} from "apisauce";
+
+const api = create({
+    baseURL: "http://jsonplaceholder.typicode.com/",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    },
+    timeout: 10000
+});
+
+api.addResponseTransform(response => {
+    if (!response.data) {
+        response.data = {};
+    }
+    const {ok, problem, data} = response;
+    if (!ok) {
+        switch (problem) {
+            case 'CLIENT_ERROR':
+                if (!data.errorMessage) {
+                    data.errorMessage = "An error has occurred. Please try again.";
+                }
+                break;
+            case 'SERVER_ERROR':
+                data.errorMessage = "There was an error with the server.";
+                break;
+            case 'TIMEOUT_ERROR':
+                data.errorMessage = "The server did not respond in time. Please try again later.";
+                break;
+            case 'CONNECTION_ERROR':
+                data.errorMessage = "Server not available. Please try again later.";
+                break;
+            case 'NETWORK_ERROR':
+                data.errorMessage = "Connection not available.";
+                break;
+            default:
+                break;
+        }
+    }
+});
